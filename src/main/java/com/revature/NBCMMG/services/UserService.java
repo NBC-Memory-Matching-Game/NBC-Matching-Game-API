0349bfc7c5ca5e4e75dc404db7f9182e31cf7b46
Robert Ni <nirobert@protonmail.com>
2021-10-26 14:12:18 -0400

    Modified UserService and added register function

--- src/main/java/com/revature/NBCMMG/services/UserService.java
+++ src/main/java/com/revature/NBCMMG/services/UserService.java
@@ -4,6 +4,7 @@ import com.revature.NBCMMG.datasources.models.User;
 import com.revature.NBCMMG.datasources.repositories.UserRepository;
 import com.revature.NBCMMG.utils.PasswordUtils;
 import com.revature.NBCMMG.utils.exceptions.AuthenticationException;
+import com.revature.NBCMMG.utils.exceptions.ResourcePersistenceException;
 import com.revature.NBCMMG.web.dtos.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class UserService {
         this.passwordUtils = passwordUtils;
     }
 
-     public boolean isUsernameTaken(String username) {
+    public boolean isUsernameTaken(String username) {
         return userRepository.findUserByUsername(username) != null;
     }
 
@@ -31,5 +32,11 @@ public class UserService {
         if (authUser == null) throw new AuthenticationException("Username and/or password is incorrect!");
 
         return new Principal(authUser);
+    }
+
+    public User register(User newUser) {
+        if (isUsernameTaken(newUser.getUsername())) throw new ResourcePersistenceException("The provided username is already taken!");
+        newUser.setPassword(passwordUtils.generateSecurePassword(newUser.getPassword()));
+        return userRepository.save(newUser);
     }
 }
---
+++ src/main/java/com/revature/NBCMMG/utils/exceptions/ResourcePersistenceException.java
@@ -0,0 +1,7 @@
+package com.revature.NBCMMG.utils.exceptions;
+
+public class ResourcePersistenceException extends RuntimeException {
+    public ResourcePersistenceException(String message) {
+        super(message);
+    }
+}
